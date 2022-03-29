550a318d425f33a8aad92177c0df5059c3bf0cc8
juangomez0120 <jmgomezortiz@gmail.com>
2022-03-28 21:21:05 -0600

    xdxdx

--- index.php
+++ index.php
@@ -1,3 +1,44 @@
+<?php
+    error_reporting(-1);
+    ini_set('display_errors', 'On');
+    set_error_handler("var_dump");
+    print_r($_POST);
+    if(isset($_POST['submit'])){
+        $nombre = $_POST['name'];
+        $email = $_POST['email'];
+        $empresa = $_POST['empresa'];
+        $message = $_POST['message'];
+
+        $to = "a00828010@tec.mx";
+        $from = "a00828010@tec.mx";
+        $subject = $nombre.' desea contactarte!';
+        $correo = "Nombre: " .$nombre;
+
+        $headers =  "From: ".($from)."\r\n";
+        $headers .= "Reply-To: ".($from)."\r\n";
+        $headers .= "Return-Path: ".($from)."\r\n";
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-type: text/html; charset=UTF-8\r\n";
+        $headers .= "X-Priority: 3\nX-MSmail-Priority: high";
+        $headers .= "X-Mailer: PHP".phpversion()."\r\n";
+
+        $result = mail($to, $subject, $message, $headers);
+
+        if($result){
+            $noti = "Correo enviado correctamente";
+        } else {
+            $noti = "Error al enviar correo.";
+        }
+
+        $notiCK = "notificacion";
+        $notiCK_value = $noti;
+        setcookie($notiCK, $notiCK_value, 0, "/"); // 86400 = 1 day
+        
+        echo $noti;
+    }
+    
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -152,7 +193,7 @@
                 <p>Agenda una llamada con nosotros y aclararemos todas tus dudas. <span>El poder de tu marca comienza contigo.</span></p>
             </div>
             <div class="col-lg-6 col-12 contactanos-box">
-                <form action="contact_get.php" method="POST">
+                <form action="" method="POST">
                     <h1 class="font-metodologia">CONTÁCTANOS</h1>
                     <label for="name">Nombre</label>
                     <input type="text" id="name" name="name" placeholder="Escribe tu nombre aquí" required>
